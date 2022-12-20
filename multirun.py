96faad2aafdaeda587601b582d3790e4395269e4
emacosta95 <emanuelecosta95@hotmail.com>
2022-12-20 19:31:10 +0100

    fix a bug in the propagation of correlation in a neural network

--- multirun.py
+++ multirun.py
@@ -1,12 +1,12 @@
 import os
 import numpy as np
 
-ls = np.arange(33,65)
-hs = [5.44]
+ls = [64]
+hs = [2.4, 3.6, 7.2, 8.4]
 # ks = [1, 3, 5, 7, 9, 11]
-ls_train = [8, 12, 18, 20]
+ls_train = [16]
 for k in ls_train:
     for l in ls:
         os.system(
-            f"nohup python run.py --model_name='2nn_ising/h_{hs[0]}_15k_l_{k}_cnn_[40, 40, 40, 40]_hc_{5}_ks_1_ps_4_nconv_0_nblock' --target_path=data/dataset_2nn/test_dataset_021222/test_periodic_{l}_l_{hs[0]}_h_100_n.npz --run_name=h_{hs[0]}_l_train_{k}_15k_augmentation_2nn_model_unet_{l}_size_4_layers_40_hc_{5}_ks_1_ps --init_path=data/dataset_2nn/test_dataset_021222/test_periodic_{l}_l_{hs[0]}_h_100_n.npz --epochs=2000 --device=cpu --num_threads=1 > output/run_l_{l}_cnn_15k.txt &"
+            f"nohup python run.py --model_name='2nn_ising/h_{hs[0]}_cnn_[40, 40, 40, 40]_hc_{5}_ks_1_ps_4_nconv_0_nblock' --target_path=data/dataset_2nn/test_unet_periodic_2nn_{l}_l_{hs[0]}_h_100_n.npz --run_name=h_{hs[0]}_15k_augmentation_2nn_model_unet_{l}_size_4_layers_40_hc_{5}_ks_1_ps --init_path=data/dataset_2nn/test_unet_periodic_2nn_{l}_l_{hs[0]}_h_100_n.npz --epochs=2000 --device=cpu --num_threads=1 > output/run_l_{l}_cnn_15k.txt &"
         )
--- multitrain.py
+++ multitrain.py
@@ -1,8 +1,8 @@
 import os
 
-# l = [24]
+l = [24]
 # data = [300000] * len(l)
-hs = [5.44]
+hs = [2.40, 3.60, 7.20, 8.40]
 # data_str = ["150k"] * len(l)
 nblocks = None
 hidden_channel = " 40 40 40 40  "
@@ -14,13 +14,11 @@ for i in range(len(hs)):
     print(
         f"nohup python train.py   --hidden_channel"
         + hidden_channel
-        + f"  --kernel_size={ks[i]} --padding={padding[i]}  --model_name='2nn_ising/h_{hs[i]}_l_multiscale"
-        + f"_cnn' --model_type={model_type} --pooling_size=1 --epochs={epochs[i]} > output/train_unet_{model_type}.txt &"
+        + f"  --kernel_size={ks[i]} --padding={padding[i]}  --model_name='2nn_ising/h_{hs[i]}_cnn' --data_path='data/dataset_2nn/train_unet_periodic_2nn_augmentation_16_l_{hs[i]:.2f}_h_30000_n.npz --model_type={model_type} --pooling_size=1 --epochs={epochs[i]} > output/train_unet_{model_type}.txt &"
     )
     os.system(
         f"nohup python train.py  --hidden_channel"
         + hidden_channel
-        + f"  --kernel_size={ks[i]} --padding={padding[i]}  --model_name='1nn_ising/h_{hs[i]}_l_multiscale"
-        + f"_cnn' --model_type={model_type} --pooling_size=1 --epochs={epochs[i]} > output/train_unet_{model_type}.txt &"
+        + f"  --kernel_size={ks[i]} --padding={padding[i]}  --model_name='2nn_ising/h_{hs[i]}_cnn' --data_path='data/dataset_2nn/train_unet_periodic_2nn_augmentation_16_l_{hs[i]:.2f}_h_30000_n.npz' --model_type={model_type} --pooling_size=1 --epochs={epochs[i]} > output/train_unet_{model_type}.txt &"
     )
     # hidden_channel = hidden_channel + " 40"
