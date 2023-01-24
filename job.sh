6bb1333935d810e0b5c817e15271474e6bba7a80
Emanuele Costa <75442035+emacosta95@users.noreply.github.com>
2023-01-24 15:11:43 +0000

    Change model_name in job.sh

--- job.sh
+++ job.sh
@@ -30,5 +30,5 @@ echo "Running on "`hostname`
 #=============================
 # running
 
-srun python train.py  --hidden_channel 40 40 40 40 40 40  --kernel_size=5 --padding=2  --model_name=h_2.7_unet_l_$SLURM_ARRAY_TASK_ID --data_path=data/1nn_ising/train_unet_periodic_augmentation_$l$\_l_2.7_h_300000_n.npz' --model_type=REDENTnopooling --pooling_size=1 --epochs=7000 
+srun python train.py  --hidden_channel 40 40 40 40 40 40  --kernel_size=5 --padding=2  --model_name=1nn_ising/h_2.7_unet_l_$SLURM_ARRAY_TASK_ID --data_path=data/1nn_ising/train_unet_periodic_augmentation_$l$\_l_2.7_h_300000_n.npz' --model_type=REDENTnopooling --pooling_size=1 --epochs=7000 
 
