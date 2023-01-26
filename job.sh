3b0f53d4fa514dd3d71fe980207fb75959b38a16
Emanuele Costa <75442035+emacosta95@users.noreply.github.com>
2023-01-26 12:31:50 +0000

    Update job.sh

--- job.sh
+++ job.sh
@@ -3,12 +3,12 @@
 #SBATCH --job-name="train_different_sizes"
 #SBATCH --time=24:00:00
 #SBATCH --mem=4G
-#SBATCH --array=30,150
+#SBATCH --array=8,2
 #SBATCH --ntasks-per-node=1
 #SBATCH --gres=gpu:4        # 1 gpus per node out of 4
 #SBATCH --partition=m100_usr_prod
-#SBATCH --output=/m100/home/userexternal/ecosta01/dft_for_ising/output/output_230123_JOB_%j.out
-#SBATCH --error=/m100/home/userexternal/ecosta01/dft_for_ising/output/error_230123_JOB_%j.out
+#SBATCH --output=/m100/home/userexternal/ecosta01/dft_for_ising/output/output_JOB_%j.out
+#SBATCH --error=/m100/home/userexternal/ecosta01/dft_for_ising/output/error_JOB_%j.out
 #SBATCH --mail-type=BEGIN,END,FAIL,TIME_LIMIT
 #SBATCH --mail-user=emanuele.costa@unicam.it
 #SBATCH --account=IscrC_SMORAGEN
@@ -23,12 +23,10 @@ echo "Running on "`hostname`
 #=============================
 # user definitions
 
-ndata=$(($((1000*$SLURM_ARRAY_TASK_ID))))
-
 
 
 #=============================
 # running
 
-srun python train.py  --hidden_channel 40 40 40 40 40 40  --kernel_size=5 --padding=2  --model_name=1nn_ising/h_2.7_unet_no_aug --data_path=data/1nn_ising/train_without_augmentation/unet_periodic_16_l_2.7_h_$ndata\_n.npz --model_type=REDENTnopooling --pooling_size=1 --epochs=3000 
+srun python train.py  --hidden_channel 40 40 40 40 40 40  --kernel_size=5 --padding=2  --model_name=1nn_ising/h_2.7_unet_no_aug_l_train_$SLURM_ARRAY_TASK_ID --data_path=data/1nn_ising/train_without_augmentation/unet_periodic_$SLURM_ARRAY_TASK_ID\_l_2.7_h_150000.npz --model_type=REDENTnopooling --pooling_size=1 --epochs=3000 
 
