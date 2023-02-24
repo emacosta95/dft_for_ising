5029995fa0fa093e1ac2c75a96e009cac640c17c
Emanuele Costa <75442035+emacosta95@users.noreply.github.com>
2023-02-24 11:50:52 +0000

    Update job.sh

--- job.sh
+++ job.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name="train_different_sizes"
 #SBATCH --time=24:00:00
 #SBATCH --mem=4G
-#SBATCH --array=12,18
+#SBATCH --array=8,12,16,18,24
 #SBATCH --ntasks-per-node=1
 #SBATCH --gres=gpu:4        # 1 gpus per node out of 4
 #SBATCH --partition=m100_usr_prod
@@ -11,7 +11,7 @@
 #SBATCH --error=/m100/home/userexternal/ecosta01/dft_for_ising/output/error_JOB_%j.out
 #SBATCH --mail-type=BEGIN,END,FAIL,TIME_LIMIT
 #SBATCH --mail-user=emanuele.costa@unicam.it
-#SBATCH --account=IscrC_SMORAGEN
+#SBATCH --account=IscrC_NEMCAQS
 
 #=============================
 # environment
@@ -28,5 +28,5 @@ echo "Running on "`hostname`
 #=============================
 # running
 
-srun python train.py  --hidden_channel 40 40 40 40 40 40   --kernel_size=5 --padding=2  --model_name=1nn_ising/h_2.7_unet_no_aug_l_train_$SLURM_ARRAY_TASK_ID --data_path=data/1nn_ising/train_without_augmentation/unet_periodic_$SLURM_ARRAY_TASK_ID\_l_2.7_h_150000_n.npz --model_type=REDENTnopooling --pooling_size=1 --epochs=3000 
+srun python train.py  --hidden_channel 40 40 40 40 40 40   --kernel_size=5 --padding=2  --model_name=1nn_ising/h_1.8_unet_no_aug_l_train_$SLURM_ARRAY_TASK_ID --data_path=data/1nn_ising/final_study_quasi_phase_transition/train_unet_periodic_$SLURM_ARRAY_TASK_ID\_l_1.8_h_150000_n.npz --model_type=REDENTnopooling --pooling_size=1 --epochs=3000 
 
