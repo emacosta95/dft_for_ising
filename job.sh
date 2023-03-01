b96f37f28f740bae0bd5adfc308ac9a89fbbb74b
Emanuele Costa <75442035+emacosta95@users.noreply.github.com>
2023-03-01 16:41:50 +0000

    Update job.sh

--- job.sh
+++ job.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name="train_different_sizes"
 #SBATCH --time=24:00:00
 #SBATCH --mem=4G
-#SBATCH --array=12,14
+#SBATCH --array=8,12,16,18,24
 #SBATCH --ntasks-per-node=1
 #SBATCH --gres=gpu:4        # 1 gpus per node out of 4
 #SBATCH --partition=m100_usr_prod
@@ -28,5 +28,5 @@ echo "Running on "`hostname`
 #=============================
 # running
 
-srun python train.py  --hidden_channel 40 40 40 40 40 40   --kernel_size=5 --padding=2  --model_name=2nn_ising/h_5.6_unet_no_aug_l_train_$SLURM_ARRAY_TASK_ID --data_path=data/2nn_ising/unet_pbc_$SLURM_ARRAY_TASK_ID\_l_5.60_h_100001_n.npz --model_type=REDENTnopooling --pooling_size=1 --epochs=3000 
+srun python train.py  --hidden_channel 40 40 40 40 40 40   --kernel_size=5 --padding=2  --model_name=1nn_ising/h_4.5_unet_no_aug_l_train_$SLURM_ARRAY_TASK_ID --data_path=data/1nn_ising/unet_periodic_$SLURM_ARRAY_TASK_ID\_l_4.5_h_150000_n.npz --model_type=REDENTnopooling --pooling_size=1 --epochs=3000 
 
