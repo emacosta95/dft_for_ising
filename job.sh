b464c36f43a67d51704452a3270578f181f1eeab
Emanuele Costa <75442035+emacosta95@users.noreply.github.com>
2023-03-10 13:22:59 +0000

    Update job.sh

--- job.sh
+++ job.sh
@@ -28,5 +28,5 @@ echo "Running on "`hostname`
 #=============================
 # running
 
-srun python train.py  --hidden_channel 40 40 40 40 40 40   --kernel_size=5 --padding=2  --model_name=1nn_ising/h_4.5_unet_no_aug_l_train_$SLURM_ARRAY_TASK_ID --data_path=data/1nn_ising/unet_periodic_$SLURM_ARRAY_TASK_ID\_l_4.5_h_150000_n.npz --model_type=REDENTnopooling --pooling_size=1 --epochs=3000 
+srun python train.py  --hidden_channel 40 40 40 40 40 40   --kernel_size=5 --padding=2  --model_name=1nn_ising/h_3.5_unet_no_aug_l_train_$SLURM_ARRAY_TASK_ID --data_path=data/1nn_ising/unet_periodic_$SLURM_ARRAY_TASK_ID\_l_3.5_h_150000_n.npz --model_type=REDENTnopooling --pooling_size=1 --epochs=3000 
 
